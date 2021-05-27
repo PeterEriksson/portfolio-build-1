b8b9a0b6afe5ca97ad06f5420f991e6a3015e582
Peter Eriksson <tuvoc1@hotmail.com>
2021-05-28 01:42:36 +0200

    testing different solutions for Work.js component
    testing different solutions for Work.js component

--- components/Work.js
+++ components/Work.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import resumeData from "../resumeData.json";
 
 function Work() {
+  const [index, setIndex] = useState(0);
+
+  const handleClick = () => {
+    if (index !== 3) {
+      setIndex((i) => i + 1);
+    } else {
+      setIndex(0);
+    }
+  };
+
   return (
     <div
       id="work"
@@ -20,27 +31,32 @@ function Work() {
           {/* Image div */}
           <div className="h-1/2">
             <LazyLoadImage
-              src={resumeData.work[0].url}
+              src={resumeData.work[index].url}
               effect="blur"
               className=""
             />
           </div>
-          {/* Project title plus desc plus tags plus button(GitHub) */}
+          {/* Project title plus description plus tags plus button(GitHub) */}
           <div className=" text-center bg-gray-50 rounded-b-xl">
             <h1 className="text-2xl font-semibold">
-              {resumeData.work[0].title}
+              {resumeData.work[index].title}
             </h1>
             <p className="text-sm font-light mx-3 mt-2">
-              {resumeData.work[0].desc}
+              {resumeData.work[index].desc}
             </p>
             <p className="text-xs font-extralight italic mt-3">
-              {resumeData.work[0].tags}
+              {resumeData.work[index].tags}
             </p>
-            <button className="bg-indigo-400 text-white w-1/4 px-2 font-light text-lg uppercase rounded-xl mt-5 mb-3 focus:outline-none">
-              github
-            </button>
+            {/*  <a
+              href="https://aftonbladet.se"
+              target="_blank"
+              className="bg-black text-white w-1/4 px-3 font-light text-xl uppercase rounded-xl mt-3 mb-4 focus:outline-none"
+            >
+              Github
+            </a> */}
           </div>
         </div>
+        <button onClick={handleClick}>next project</button>
       </div>
     </div>
   );
