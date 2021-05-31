6e8d665544b76267fabf2b82b0d429b51b739a05
Peter Eriksson <tuvoc1@hotmail.com>
2021-06-01 00:02:06 +0200

    Work component update
    Work component update

--- components/Work.js
+++ components/Work.js
@@ -29,7 +29,7 @@ function Work() {
   return (
     <div
       id="work"
-      className="min-h-screen flex flex-col items-center justify-center"
+      className="min-h-screen flex flex-col items-center justify-center "
     >
       <div
         aria-label="Heading text"
@@ -39,39 +39,48 @@ function Work() {
         <h3 className="text-xl font-extralight">Some of my work</h3>
       </div>
 
-      <div className="flex flex-col justify-center items-center">
-        <LazyLoadImage
-          src={resumeData.work[index].url}
-          effect="blur"
-          className="rounded-xl "
-          height="300px"
-          width="300px"
-          /* key={i} */
-        />
-        <h2 className="text-xl font-bold">{resumeData.work[index].title}</h2>
-        <p className="max-w-sm text-center text-sm font-light">
-          {resumeData.work[index].desc}
-        </p>
-        <a
-          href={resumeData.work[index].githubUrl}
-          className="font-light text-sm mt-3"
+      {resumeData.work.map((item, i) => (
+        <div
+          className={`${
+            i === index
+              ? "flex flex-col justify-center items-center opacity-1 transition duration-700 ease-in"
+              : "opacity-0 absolute"
+          }`}
         >
-          Github
-        </a>
-        <p className="text-xs font-extralight italic mt-3">
-          {resumeData.work[index].tags}
-        </p>
-        <div className="flex mt-4">
-          <ArrowCircleLeftIcon
-            onClick={handleClickLeft}
-            className="h-8 cursor-pointer"
+          <LazyLoadImage
+            src={resumeData.work[index].url}
+            /* effect="blur" */
+            className="rounded-xl "
+            height="300px"
+            width="300px"
+            /* key={i} */
           />
-          <ArrowCircleRightIcon
-            onClick={handleClickRight}
-            className="h-8 cursor-pointer"
-          />
+          <h2 className="text-xl font-bold">{resumeData.work[index].title}</h2>
+          <p className="max-w-sm text-center text-sm font-light transition duration-150 ease-in-out transform">
+            {resumeData.work[index].desc}
+          </p>
+          <a
+            href={resumeData.work[index].githubUrl}
+            className="font-light text-sm mt-3 hover:underline"
+          >
+            Github
+          </a>
+          <p className="text-xs font-extralight italic mt-3">
+            {resumeData.work[index].tags}
+          </p>
+
+          <div className="flex mt-4">
+            <ArrowCircleLeftIcon
+              onClick={handleClickLeft}
+              className="h-8 cursor-pointer"
+            />
+            <ArrowCircleRightIcon
+              onClick={handleClickRight}
+              className="h-8 cursor-pointer"
+            />
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
