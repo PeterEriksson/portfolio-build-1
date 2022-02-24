77c8a025db8608f4bd763c6ad1d77d7a4a3acaa0
Peter Eriksson <tuvoc1@hotmail.com>
2022-02-24 15:29:47 +0100

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -23,34 +23,34 @@ function Contact() {
           } transform transition duration-1000 //mr-5 ease-in-out flex text-white w-1/2`}
         >
           <div className="flex flex-col //pl-5">
-            <h1 className="xs:text-5xl text-3xl text-white font-bold mr-10 mb-4">
+            <h1 className="xs:text-5xl text-3xl text-white font-bold //mr-10 mb-2 xs:mb-4">
               Ping me!
             </h1>
-            <p className="xs:text-base text-sm">
-              I am Peter Eriksson, a self-taught front end-developer. I am
-              especially in to React, where I've built several pojects. Always
-              eager to learn new stuff. Fluent in Swedish and English. 75
-              university credits computer science.
+            <p className="xs:text-base text-sm   //picture-covering-mail-text: w-10/12 xs:w-full">
+              I am Peter Eriksson, a self-taught front end-developer, especially
+              in to React.js. Always eager to learn new stuff. Fluent in Swedish
+              and English.
             </p>
+            {/*  <p>75 university credits computer science.</p> */}
 
             {/*Contact info section  */}
-            <section className="flex flex-col lg:flex-row lg:items-center mt-2.5 space-y-2 lg:space-y-0 lg:space-x-3">
+            <section className="flex flex-col lg:flex-row lg:items-center space-y-2 lg:space-y-0 lg:space-x-3 //picture-covering-mail-text: mt-6 xs:mt-2.5">
               <div>
-                <h3 className="text-lg font-bold">Phone</h3>
-                <div className="flex items-center space-x-2">
-                  <PhoneIcon className="h-5 w-5" />
-                  <p>070-41...39..</p>
+                <h3 className="xs:text-lg font-bold">Phone</h3>
+                <div className="flex items-center xs:space-x-2">
+                  <PhoneIcon className="xs:h-5 xs:w-5  w-3 h-5" />
+                  <p className="text-sm">070-41...39..</p>
                 </div>
               </div>
               <div>
-                <h3 className="text-lg font-bold">Mail</h3>
-                <div className="flex items-center space-x-2">
-                  <MailIcon className="h-5 w-5" />
-                  <p>peter...@hotmail.com</p>
+                <h3 className="xs:text-lg font-bold">Mail</h3>
+                <div className="flex items-center xs:space-x-2">
+                  <MailIcon className="xs:h-5 xs:w-5  w-3 h-5" />
+                  <p className="text-sm">peter...@hotmail.com</p>
                 </div>
               </div>
               <div>
-                <h3 className="text-lg font-bold">Linkedin</h3>
+                <h3 className="xs:text-lg font-bold">Linkedin</h3>
                 <img
                   className="h-6 w-6 rounded-lg lg:mx-auto"
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/1200px-Linkedin_icon.svg.png"
@@ -58,7 +58,7 @@ function Contact() {
                 />
               </div>
               <div>
-                <h3 className="text-lg font-bold">Github</h3>
+                <h3 className="xs:text-lg font-bold">Github</h3>
                 <img
                   className="h-6 w-6 rounded-lg lg:mx-auto"
                   src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
@@ -70,11 +70,11 @@ function Contact() {
         </div>
 
         {/* right image container */}
-        <div className="//pr-4">
+        <div className="//picture-covering-mail-text: pb-14 xs:pb-0">
           <LazyLoadImage
             src={resumeData.header.imgGuinness2}
             effect="blur"
-            className="rounded-xl "
+            className="rounded-xl  "
             /* height="350px"
           width="200px" */
           />
